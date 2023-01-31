621eac9620105a0a0d25eb0df10b5cfc791812c7
Rakesh <rakesh33d@gmail.com>
2023-01-31 21:38:25 +0530

    solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,66 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int cheese;
+    private int toppings;
+    private boolean extracheeseadded;
+    private boolean extratoppingsadded;
+    private boolean istakeawayadded;
+    private int baseprice;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(this.isVeg==true){
+            this.baseprice=300;
+            this.price=300;
+            this.toppings=70;
+        }
+        else{
+            this.baseprice=400;
+            this.price=400;
+            this.toppings=120;
+        }
+        this.cheese=80;
+        this.extracheeseadded=false;
+        this.extratoppingsadded=false;
+        this.istakeawayadded=false;
+        this.bill="";
     }
+
 
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+       if(extracheeseadded==false){
+           this.price = price + cheese;
+           extracheeseadded=true;
+       }
+
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(extratoppingsadded==false){
+            this.price = price + toppings;
+            extratoppingsadded=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(istakeawayadded==false){
+            this.price = price + 20;
+            istakeawayadded=true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        bill+= "Base Price Of The Pizza: "+baseprice+"\n";
+        if(extracheeseadded)bill+="Extra Cheese Added: 80"+"\n";
+        if(extratoppingsadded)bill+="Extra Toppings Added: "+toppings+"\n";
+        if(istakeawayadded)bill+="Paperbag Added: 20"+"\n";
+        bill+="Total Price: "+price+"\n";
         return this.bill;
     }
 }
